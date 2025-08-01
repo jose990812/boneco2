241a9b234a57c15ca68484a89b5f9e2a904868bb
jose990812 <ailton.bbb.brh@gmail.com>
2025-08-01 17:10:01 -0300

    sadas

---
+++ boneco.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+
+@app.route("/add")
+
+def pagina_secundaria():
+    return render_template("tela2.html")
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render define a variável PORT
+    app.run(host='0.0.0.0', port=port)
+    
